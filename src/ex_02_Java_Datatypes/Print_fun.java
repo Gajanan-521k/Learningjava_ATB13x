b25e7bb4873fae340a43dabf10c0b22e6f5a34f2
GAJANAN <gajanan2996@gmail.com>
2025-06-14 21:13:34 +0530

    Printf

---
+++ src/ex_02_Java_Datatypes/Print_fun.java
@@ -0,0 +1,29 @@
+package ex_02_Java_Datatypes;
+
+public class Print_fun {
+    public static void main(String[] args) {
+        int a = 9;
+
+        int b1 = 1, c1 = a * b1;
+        int b2 = 2, c2 = a * b2;
+        int b3 = 3, c3 = a * b3;
+        int b4 = 4, c4 = a * b4;
+        int b5 = 5, c5 = a * b5;
+        int b6 = 6, c6 = a * b6;
+        int b7 = 7, c7 = a * b7;
+        int b8 = 8, c8 = a * b8;
+        int b9 = 9, c9 = a * b9;
+        int b10 = 10, c10 = a * b10;
+
+        System.out.printf("%d * %d = %d\n", a, b1, c1);
+        System.out.printf("%d * %d = %d\n", a, b2, c2);
+        System.out.printf("%d * %d = %d\n", a, b3, c3);
+        System.out.printf("%d * %d = %d\n", a, b4, c4);
+        System.out.printf("%d * %d = %d\n", a, b5, c5);
+        System.out.printf("%d * %d = %d\n", a, b6, c6);
+        System.out.printf("%d * %d = %d\n", a, b7, c7);
+        System.out.printf("%d * %d = %d\n", a, b8, c8);
+        System.out.printf("%d * %d = %d\n", a, b9, c9);
+        System.out.printf("%d * %d = %d\n", a, b10, c10);
+    }
+}
