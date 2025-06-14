fb45bd8c34cd4c38d456e2f44fdcc803249bea7a
GAJANAN <gajanan2996@gmail.com>
2025-06-14 18:06:37 +0530

    14 june2025

--- src/ex_01_Java_Basics/Lab_003_Comments.java
+++ src/ex_01_Java_Basics/Lab_003_Comments.java
@@ -2,8 +2,6 @@ package ex_01_Java_Basics;
 
 public class Lab_003_Comments {
     public static void main(String[] args) {
-        /*System.out.println("Hi");
-        System.out.println("Hi");
-        System.out.println("Hi");
-    }*/
+     System.out.println("Hi");
+    }
 }
